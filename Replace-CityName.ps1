a7fa4bf40d268855aebda395bc23aca9a8f6b33b
Kristopher Roy <kristopher.roy@gmail.com>
2023-07-31 15:46:56 -0700

    fixed unicode issue with losing special characters

--- Replace-CityName.ps1
+++ Replace-CityName.ps1
@@ -11,7 +11,7 @@
 #The headers in this file must be "Service Name, Slug, Heading, SubHeading, Content 1, CTA Heading, CTA SubHeading, Content 2" If these headers change, then the script will need to be updated
 $csvwebfile = import-csv 'C:\Projects\Kova\Floor Care Installation - Hardwood Floor Refinishing Cities (4).csv'
 
-#This is the csv file with the list of cities to place in the file replace the foler and file path of this file
+#This is the csv file with the list of cities to place in the file replace the folder and file path of this file
 #The header in this file must be "Service Name" 
 $NewCityList = import-csv 'C:\Projects\Kova\city names - Floor Care Installation - Hardwood Floor Refinishing Cities (5).csv'
 
@@ -39,6 +39,9 @@ Foreach($city in $NewCityList)
     $csvwebfile.'Content 2' = $csvwebfile.'Content 2' -creplace $oldcitylower,$newcitylower
     $csvwebfile.'Content 2' = $csvwebfile.'Content 2' -creplace $oldcity,$newcityUpper
     
-    #Exports the content into a new csv file named whatever the new city is -export.csv
-    $csvwebfile|export-csv C:\Projects\Kova\$newcitylower-export.csv -NoTypeInformation
+    #Exports the content into a new individul csv files named whatever the new city is -export.csv
+    #$csvwebfile|export-csv C:\Projects\Kova\$newcitylower-export.csv -NoTypeInformation -Encoding Unicode
+
+    #Exports the content into a new individul csv file named newcities-export.csv
+    $csvwebfile|export-csv "C:\Projects\Kova\newcities-export.csv" -NoTypeInformation -Encoding Unicode -append
 }
